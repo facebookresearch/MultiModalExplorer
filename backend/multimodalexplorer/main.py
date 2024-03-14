f56831f61d6d391fd4f1370d56717e1943e8b293
MaryAnn Chukwuka <annychuks07@gmail.com>
2024-03-14 01:33:38 +0100

    create mock api to transmit points to client

--- backend/multimodalexplorer/main.py
+++ backend/multimodalexplorer/main.py
@@ -4,10 +4,28 @@
 # LICENSE file in the root directory of this source tree.
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from multimodalexplorer.api.endpoints import create_router
 
 app = FastAPI()
+
+# Configure CORS
+
+allowed_origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins, 
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to MultiModalExplorer."}
+
+app.include_router(create_router())
 
